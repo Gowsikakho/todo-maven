0b08182100ef6d97f09cc9c0fb177f85925a7954
Gowsikakho <arulgowsi3357@gmail.com>
2025-09-16 13:49:45 +0530

    DBconnection established

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,1 +1,17 @@
+package com.todo;
 
+import com.todo.util.DatabaseConnection;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabaseConnection db_Connection = new DatabaseConnection();
+        try {
+            Connection cn=db_Connection.getDBConnection();
+            System.out.println("Database connection established successfully.");
+        } catch (SQLException e) {
+            System.out.println("Failed to establish database connection: " + e.getMessage());
+        }
+    }
+}
