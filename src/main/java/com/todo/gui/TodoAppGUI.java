7c81dd30f354d27e61d972ccb4e6ffdd33e67308
Gowsikakho <gowsikakho@gmail.com>
2025-09-17 12:24:48 +0530

    gui_setup

---
+++ src/main/java/com/todo/gui/TodoAppGUI.java
@@ -0,0 +1,70 @@
+package com.todo.gui;
+
+import com.todo.dao.TodoAppDAO;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TodoAppGUI extends JFrame {
+    private TodoAppDAO todoDAO;
+    private JTable todoTable;
+    private DefaultTableModel tableModel;
+    private JTextField titleField;
+    private JTextArea descriptionArea;
+    private JCheckBox completedCheckBox;
+    private JButton addButton;
+    private JButton updateButton;
+    private JButton deleteButton;
+    private JButton refreshButton;
+    private JComboBox<String> filterComboBox;
+
+    public TodoAppGUI(){
+        this.todoDAO = new TodoAppDAO();
+    }
+
+    private void initializeComponents() {
+        setTitle("Todo Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+
+        String[] columnNames = {"ID", "Title", "Description", "Created At", "Completed", "Updated At"};
+        tableModel = new DefaultTableModel(columnNames, 0){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+
+        todoTable = new JTable(tableModel);
+        todoTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        todoTable.getSelectionModel().addListSelectionListener(
+            (e)-> {
+                if(!e.getValueIsAdjusting()){
+                    //loadSelectedTodo();
+                }
+            }
+        );
+
+        titleField = new JTextField(20);
+        descriptionArea = new JTextArea(3, 20);
+        descriptionArea.setLineWrap(true);
+        descriptionArea.setWrapStyleWord(true);
+        completedCheckBox = new JCheckBox("Completed");
+        addButton = new JButton("Add Todo");
+        updateButton = new JButton("Update Todo");
+        deleteButton = new JButton("Delete Todo");
+        refreshButton = new JButton("Refresh Todo");
+
+        String[] filterOptions = {"All", "Completed", "Pending"};
+        filterComboBox = new JComboBox<>(filterOptions);
+        filterComboBox.addActionListener((e)->{
+            //filterTodos();
+        });
+    }
+
+}
